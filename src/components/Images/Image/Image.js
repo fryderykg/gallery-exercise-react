b676f60c64da759d33b6fb11668846cbd44eb577
FryderykG <eszkimos@gmail.com>
2017-12-16 16:24:16 +0100

    build images from image data from Gallery component

--- src/components/Images/Image/Image.js
+++ src/components/Images/Image/Image.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './image.css';
 
 const Image = props => {
   return (
-    <div>
-      <img src='https://placehold.it/250x150'/>
+    <div className='Image'>
+      <img src={props.imgSrc}
+           alt={props.imgAlt}/>
+      <div className="Image__description">
+        <div>{props.title}</div>
+        <div>{props.date}</div>
+      </div>
     </div>
   );
 };
 
 Image.propTypes = {
+  key: PropTypes.string,
+  imgSrc: PropTypes.string,
+  imgAlt: PropTypes.string,
+  title: PropTypes.string,
+  date: PropTypes.string
 
 };
 
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './images.css';
 
 import Image from './Image/Image';
 
 const Images = props => {
+  let images = [];
+
+  for (let i = 0; i < props.images.length; i++) {
+    images.push(
+      <Image key={props.images[i].id}
+             imgSrc={props.images[i].imageRelativePath}
+             imgAlt={props.images[i].name}
+             title={props.images[i].name}
+             date={props.images[i].timestamp}
+      />
+    )
+  }
+
+
   return (
-    <div>
-      <Image/>
-      <Image/>
-      <Image/>
+    <div className='Images'>
+      {images}
     </div>
   );
 };
 
 Images.propTypes = {
-
+  images: PropTypes.array
 };
 
 export default Images;
